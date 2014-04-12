476dd2afe9ca2f67f1e7a84af824a53efb3b72dc
Christian Bundy <me@christianbundy.com>
2014-04-12 13:23:14 -0700

    Add basic implementation

---
+++ index.js
@@ -0,0 +1,30 @@
+request = require('request');
+
+var api = {}
+
+// make a request with the specified query
+api.call = function (query, cb) {
+  var fullQuery = 'https://hn.algolia.com/api/v1/' + query;
+  request(fullQuery, function (error, response, body) {
+    if (!error && !response.statusCode == 200)
+      error = response.statusCode;
+    if (typeof body !== 'undefined')
+      body = JSON.parse(body);
+    cb(error, body);
+  });
+}
+
+//
+api.getItem = function (id, cb) {
+  api.call('items/' + id, cb);
+}
+
+api.getUser = function (username, cb) {
+  api.call('users/' + username, cb)
+}
+
+api.getUser('ChristianBundy', function (error, data) {
+  if (error)
+    throw error;
+  console.log(data);
+});
