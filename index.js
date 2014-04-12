588dafd594279d1837fea6921afe843052ee0446
Christian Bundy <me@christianbundy.com>
2014-04-12 13:59:44 -0700

    Restructure index.js to create api object

--- index.js
+++ index.js
@@ -1,73 +1,64 @@
 request = require('request');
 
-var api = {}
+var api = {
+  // make a request to the specified endpoint
+  call: function (endpoint, cb) {
+    var query = 'https://hn.algolia.com/api/v1/' + endpoint;
+    request(query, function (error, response, body) {
+      if (!error && !response.statusCode == 200)
+        error = response.statusCode;
+      if (typeof body !== 'undefined')
+        body = JSON.parse(body);
+      cb(error, body);
+    });
+  },
 
-// make a request with the specified query
-api.call = function (query, cb) {
-  var fullQuery = 'https://hn.algolia.com/api/v1/' + query;
-  request(fullQuery, function (error, response, body) {
-    if (!error && !response.statusCode == 200)
-      error = response.statusCode;
-    if (typeof body !== 'undefined')
-      body = JSON.parse(body);
-    cb(error, body);
-  });
-}
+  // basic searches
+  getItem: function (id, cb) {
+    api.call('items/' + id, cb);
+  },
 
-// basic searches
-api.getItem = function (id, cb) {
-  api.call('items/' + id, cb);
-}
+  getUser: function (username, cb) {
+    api.call('users/' + username, cb)
+  },
 
-api.getUser = function (username, cb) {
-  api.call('users/' + username, cb)
-}
+  getLastStories: function (cb) {
+    api.call('search_by_date?tags=story', cb);
+  },
 
-api.getLastStories = function (cb) {
-  api.call('search_by_date?tags=story', cb);
-}
+  getLastPolls: function (cb) {
+    api.call('search_by_date?tags=poll', cb);
+  },
 
-api.getLastPolls = function (cb) {
-  api.call('search_by_date?tags=poll', cb);
-}
+  getLastPosts: function (cb) {
+    api.call('search_by_date?tags=(story,poll)', cb);
+  },
 
-api.getLastPosts = function (cb) {
-  api.call('search_by_date?tags=(story,poll)', cb);
-}
+  getUserStories: function (username, cb) {
+    api.call('search?tags=story,author_' + username, cb);
+  },
 
-api.getUserStories = function (username, cb) {
-  api.call('search?tags=story,author_' + username, cb);
-}
+  searchStories: function (search, cb) {
+    api.call('search?query=' + search + '&tags=story', cb);
+  },
 
-api.searchStories = function (search, cb) {
-  api.call('search?query=' + search + '&tags=story', cb);
-}
+  searchPolls: function (search, cb) {
+    api.call('search?query=' + search + '&tags=poll', cb);
+  },
 
-api.searchPolls = function (search, cb) {
-  api.call('search?query=' + search + '&tags=poll', cb);
-}
-
-api.search = function (obj, cb) {
-  var first = true;
-  var params = '';
-  for (var key in obj) {
-    if (first) {
-      params += '?';
-      first = false;
-    } else {
-      params += '&';
+  search: function (obj, cb) {
+    var first = true;
+    var params = '?';
+    for (var key in obj) {
+      if (first) {
+        first = false;
+      } else {
+        params += '&';
+      }
+      params += key + '=' + obj[key];
     }
-    params += key + '=' + obj[key];
+    api.call('search' + params, cb);
   }
-  api.call('search' + params, cb);
-}
+};
 
-
-api.search({
-  query: 'javascript',
-  tags: 'poll'
-}, function (error, data) {
-  if (error)
-    throw error;
-  console.log(data);
-});
+module.exports = api;
