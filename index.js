eb840ecb19a07b72620181904d1a3bdfd318116d
beltex <cse93251@cse.yorku.ca>
2014-06-09 21:56:14 -0400

    Some fixies and cleanup. Add item and user methods back

--- index.js
+++ index.js
@@ -16,21 +16,19 @@ var request = require('request');
 ////////////////////////////////////////////////////////////////////////////////
 
 
+/**
+ * Max number of hits (results) for a query - set by Algolia
+ */
 var MAX_HITS_PER_PAGE = '1000';
 
+
+/**
+ * Request types
+ */
+var TYPE_ITEM = 'items';
+var TYPE_USER = 'users';
 var TYPE_SEARCH = 'search';
 var TYPE_SEARCH_BY_DATE = 'search_by_date';
-
-var FUNCTIONS_TAG = ["story",
-                     "comment",
-                     "poll",
-                     "pollop",
-                     "show_hn",
-                     "ask_hn",
-                     "author"];
-
-var FUNCTIONS_FILTER = ["top", "recent"];
-var FUNCTIONS_TIME = ["since", "before"];
 
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -45,20 +43,31 @@ function encodeURIComponentArray(arr) {
 }
 
 
-function numericFilter(caller, marker, obj) {
-  // Either 'before' or 'since'
-  var sym = (caller === 'before') ? '<=' : '>=';
-  var temp = 'created_at_i' + sym +  timestamp(marker);
+/**
+ * Generate the numeric filter
+ *
+ * @param caller What is the name of the calling function?
+ * @param marker What is the date range marker?
+ */
+function numericFilters(caller, marker) {
+  switch (caller) {
+    case 'before':
+      var sym = '<=';
+    case 'since':
+      var sym = '>=';
+  }
 
-  // Don't set a timestamp for this case, better performance
-  obj.numericFilters = (marker === 'forever') ? '' : temp;
-  return obj;
+  // Don't set a timestamp incase of forever, better performance
+  var numericFilters = (marker === 'forever') ? '' : 'created_at_i' + sym +  timestamp(marker);
+  return numericFilters;
 }
 
 
-// Based on Algolia's own hnsearh.js
-// https://github.com/algolia/hn-search
-function timestamp(since) {
+/**
+ * Generate Unix timestamp based on date range marker. Based on Algolia's own
+ * hnsearh.js. See repo for more: https://github.com/algolia/hn-search
+ */
+function timestamp(range) {
   var now = new Date(); 
   var now_utc = Date.UTC(now.getUTCFullYear(),
                          now.getUTCMonth(),
@@ -67,7 +76,7 @@ function timestamp(since) {
                          now.getUTCMinutes(),
                          now.getUTCSeconds()) / 1000;
   
-  switch (since) {
+  switch (range) {
     case 'past_24h':
       return (now_utc - (24 * 60 * 60));
     case 'past_week':
@@ -85,16 +94,16 @@ function timestamp(since) {
 
 var hn = function() {
  
-  this.tags = {tags: '',
-               hitsPerPage: MAX_HITS_PER_PAGE};
+  this.tags_or = { };
+  this.tags = {hitsPerPage: MAX_HITS_PER_PAGE};
 
   // Make a request with the specified uri component array
   // and query argument object. If the queryObj is omitted,
   // it will be assumed to be empty and the callback may be 
   // there instead
   this.call = function (components, queryObj, cb) {
-    if(!Array.isArray(components)) components = [components];
-    if(typeof queryObj === 'function') {
+    if (!Array.isArray(components)) components = [components];
+    if (typeof queryObj === 'function') {
       cb = queryObj;
       queryObj = {};
     }
@@ -105,14 +114,17 @@ var hn = function() {
     var query_args = querystring.stringify(queryObj);
     if(query_args.length > 0) query += '?' + query_args;
 
+    console.log(query);
     request(query, function (error, response, body) {
-      if (!error && response.statusCode != 200)
+      if (!error && response.statusCode != 200) { 
         error = response.statusCode;
+      }
+
       if (typeof body !== 'undefined') {
         try {
           body = JSON.parse(body);
-        } catch(ex) {
-          if(!error) error = ex;
+        } catch (ex) {
+          if (!error) error = ex;
         }
       }
       cb(error, body);
@@ -127,33 +139,41 @@ module.exports = new hn();
 ////////////////////////////////////////////////////////////////////////////////
 
 
+var FUNCTIONS_TAG = ['story','comment','poll','pollop','show_hn','ask_hn','author'];
 FUNCTIONS_TAG.forEach(function (fName) {
   hn.prototype[fName] = function (id) {
-    // 'author' or 'story' with id
-    if (arguments.length === 1) {
-        fName = fName + "_" + id;
+
+    if (arguments.length === 1 && (fName === 'author' || fName === 'story')) {
+        // Have an arg to deal with, either the author (username) or story id
+        // This tag is ANDed 
+        fName = fName + '_' + id;
+        this.tags.tags = (this.tags.tags === '') ? fName : this.tags.tags + ',' + fName;
+    }
+    else {
+        // Or these tags
+        this.tags_or.tags = (typeof this.tags_or.tags === 'undefined') ? fName : this.tags_or.tags + ',' + fName;
     }
 
-    this.tags.tags = (this.tags.tags === '') ? fName :
-                                               this.tags.tags + "," + fName;
     return this;
   };
 });
 
 
+var FUNCTIONS_FILTER = ['top', 'recent'];
 FUNCTIONS_FILTER.forEach(function (fName) {
   hn.prototype[fName] = function (cb) {
     // Set search type 
     switch (fName) {
-      case "top":
+      case 'top':
         this.type = TYPE_SEARCH;
         break;
-      case "recent":
+      case 'recent':
         this.type = TYPE_SEARCH_BY_DATE;
         break;
     }
 
     if (arguments.length === 1 && typeof cb === 'function') {
+        this.tags.tags = this.tags.tags + '(' + this.tags_or.tags + ')';
         this.call(this.type, this.tags, cb);
     }
     else {
@@ -163,11 +183,14 @@ FUNCTIONS_FILTER.forEach(function (fName) {
 });
 
 
+var FUNCTIONS_TIME = ['since', 'before'];
 FUNCTIONS_TIME.forEach(function (fName) {
   hn.prototype[fName] = function(marker, cb) {
-    numericFilter(fName, marker, this.tags);
+    this.tags.numericFilters = numericFilters(fName, marker);
 
     if (arguments.length === 2 && typeof cb === 'function') {
+      this.tags.tags = this.tags.tags + '(' + this.tags_or.tags + ')';
+      this.
       this.call(this.type, this.tags, cb);
     }
     else {
@@ -177,11 +200,29 @@ FUNCTIONS_TIME.forEach(function (fName) {
 });
 
 
+var FUNCTIONS_SINGLE = ['item', 'user'];
+FUNCTIONS_SINGLE.forEach(function (fName) {
+  hn.prototype[fName] = function(id, cb) {
+    switch (fName) {
+      case 'item':
+        this.type = TYPE_ITEM;
+        break;
+      case 'user':
+        this.type = TYPE_USER;
+        break;
+    }
+    this.tags.tags = this.tags.tags + '(' + this.tags_or.tags + ')';
+    this.call([this.type, id], cb);
+  };
+});
+
+
 hn.prototype.search = function (query, cb) {
   this.tags.query = query; 
 
   if (arguments.length === 2 && typeof cb === 'function') {
     if (typeof this.type === 'undefined') { this.type = TYPE_SEARCH }
+    this.tags.tags = '(' + this.tags_or.tags + ')';
     this.call(this.type, this.tags, cb);
   }
   else {
